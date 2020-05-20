acf03b8e2175bd4a5a35a7c4c4e455444ef229c0
bourntec <bourntec@BRN-2005-001>
2020-05-20 12:36:57 +0530

    material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginService } from './login/login.service';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import {DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { WjGridModule } from '@grapecity/wijmo.angular2.grid';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-  ],
+    UserComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    WjGridModule,
+    FormsModule,
+    BrowserAnimationsModule
+
   ],
   providers: [LoginService],
   bootstrap: [AppComponent]
