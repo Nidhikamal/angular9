317f787bfdf9e38ea8f926e734a940be6317a2a8
BOURNTEC <srijini.t@bourntec.com>
2020-06-04 13:17:55 +0530

    theme change

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,33 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
-import { MatButtonModule,} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import{ MatListModule} from '@angular/material/list';
-import{ MatInputModule } from '@angular/material/input';
-import{ MatTableModule } from '@angular/material/table';
-import{ MatToolbarModule } from '@angular/material/toolbar';
-import{ MatBadgeModule } from '@angular/material/badge';
-import{ MatIconModule } from '@angular/material/icon';
-import{ MatSidenavModule } from '@angular/material/sidenav';
-import{ MatSelectModule } from '@angular/material/select';
-import{ MatDatepickerModule } from '@angular/material/datepicker';
-import{ MatRadioModule } from '@angular/material/radio';
-import{ MatNativeDateModule } from '@angular/material/core';
-import{ MatChipsModule } from '@angular/material/chips';
-import{ MatTooltipModule } from '@angular/material/tooltip';
-import{  MatPaginatorModule } from '@angular/material/paginator';
-import{ MatMenuModule } from '@angular/material/menu';
-import{ MatDividerModule } from '@angular/material/divider';
-import{ MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import{ MatCardModule } from '@angular/material/card';
-import{ MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 
-         
+
 
 @NgModule({
    imports: [
