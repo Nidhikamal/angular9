7c92a1385bf0da240dd5277016cdbbaedd8dbc4c
Prashith <prashith.c@gmail.com>
2020-06-25 11:10:14 +0530

    Wijmo Export PDF Functionality

--- src/app/components/flex-grid/flex-grid.component.ts
+++ src/app/components/flex-grid/flex-grid.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CollectionView } from '@grapecity/wijmo';
 import * as wjcOData from '@grapecity/wijmo.odata';
 import { Popup } from 'src/app/shared/popupmethod';
+import * as wijmo from '@grapecity/wijmo';
+import * as grid from '@grapecity/wijmo.grid';
+import * as pdf from '@grapecity/wijmo.pdf';
+import * as gridPdf from '@grapecity/wijmo.grid.pdf';
+import '@grapecity/wijmo.styles/wijmo.css';
 
 @Component({
   selector: 'app-flex-grid',
@@ -9,8 +14,61 @@ import { Popup } from 'src/app/shared/popupmethod';
   styleUrls: ['./flex-grid.component.css']
 })
 export class FlexGridComponent implements OnInit {
+
+  // Reference enumerations to use them in markup.
+  ScaleModeEnum = gridPdf.ScaleMode;
+  PdfPageOrientationEnum = pdf.PdfPageOrientation;
+  ExportModeEnum = gridPdf.ExportMode;
+  
+  scaleMode = gridPdf.ScaleMode.ActualSize;
+  orientation = pdf.PdfPageOrientation.Portrait;
+  exportMode = gridPdf.ExportMode.All;
   data: CollectionView;
   datas: wjcOData.ODataCollectionView;
+
+  //flex ID is fetch here
+  @ViewChild('flex') flexGrid: grid.FlexGrid;
+  //
+  
+
+  //Export PDF Function
+  export() {
+    gridPdf.FlexGridPdfConverter.export(this.flexGrid, 'FlexGrid.pdf', {
+        maxPages: 10,
+        exportMode: this.exportMode,
+        scaleMode: this.scaleMode,
+        documentOptions: {
+            pageSettings: {
+                layout: this.orientation
+            },
+            header: {
+                declarative: {
+                    text: '\t&[Page]\\&[Pages]'
+                }
+            },
+            footer: {
+                declarative: {
+                    text: '\t&[Page]\\&[Pages]'
+                }
+            }
+        },
+        styles: {
+            cellStyle: {
+                backgroundColor: '#ffffff',
+                borderColor: '#c6c6c6'
+            },
+            altCellStyle: {
+                backgroundColor: '#f9f9f9'
+            },
+            groupCellStyle: {
+                backgroundColor: '#dddddd'
+            },
+            headerCellStyle: {
+                backgroundColor: '#eaeaea'
+            }
+        }
+    });
+  }
 
   constructor( private popupcomp: Popup) { 
     this.data = this._getData(), {
@@ -38,7 +96,8 @@ export class FlexGridComponent implements OnInit {
     }
     return new CollectionView(data, {
       pageSize: 3
-  });
+  });  
+
 }
   ngOnInit(): void {
   }
